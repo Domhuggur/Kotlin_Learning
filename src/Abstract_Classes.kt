1e37ecd10bb21194e3f9b0fcb798c9bdb9abf52d
domhuggur <correojacobo@gmail.com>
2022-09-07 17:46:25 +0200

    Starting!

---
+++ src/Abstract_Classes.kt
@@ -0,0 +1,2 @@
+abstract class Abstract_Classes {
+}
---
+++ src/Functions.kt
@@ -0,0 +1,11 @@
+class Functions{
+    fun whatTypeOfVariableItIs(type:Any):String{
+        return when(type){
+            is String -> "It's a variable of type String"
+            is Int -> "It's a variable of type Int"
+            is Float -> "It's a variable of type  Float"
+            is Boolean -> "It's a variable of type  Boolean"
+            else -> "It isn't a variable of known type"
+        }
+    }
+}
